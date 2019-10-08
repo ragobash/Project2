39133fdb6d67401290e158e5ac2c05988f0e48ca
Jose Marin Hernandez <jmhern25@yahoo.com>
2019-10-08 18:55:25 -0500

    pushing to my branch for update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
       res.render("index");
-      console.log(req);
   });
 
   //This route is served when the user clicks the "Quick Play" button and is sent to the form.
@@ -13,8 +12,8 @@ module.exports = function(app) {
   });
 
 
-  app.get("/play/result", function(req, res) {
-      res.render("result");
+  app.get("/results", function(req, res) {
+        res.render("result");
   });
 
   // app.get("*", function(req, res) {
